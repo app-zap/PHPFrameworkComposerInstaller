4248132671a4e1c3369f0517d79b18811728be45
Sebastian Michaelsen <sebastian@app-zap.de>
2015-08-19 23:47:58 +0200

    Update Plugin.php

--- Classes/Plugin.php
+++ Classes/Plugin.php
@@ -4,9 +4,23 @@ namespace AppZap\PHPFrameworkComposerInstaller;
 use Composer\Composer;
 use Composer\IO\IOInterface;
 use Composer\Plugin\PluginInterface;
+use Composer\Script\Event;
+use Composer\Script\ScriptEvents;
 
 class Plugin implements PluginInterface {
+
     public function activate(Composer $composer, IOInterface $io) {
-        
+        $io->write('activated');
     }
+
+    public static function getSubscribedEvents() {
+        return [
+          ScriptEvents::POST_INSTALL_CMD => 'postInstall',
+        ];
+    }
+
+    public function postInstall(Event $event) {
+        echo 'caught event:' . $event->getName();
+    }
+
 }
