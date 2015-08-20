493c020b0f17300dbf80991592edadef5a885e54
Sebastian Michaelsen <sebastian@michaelsen.io>
2015-08-20 08:32:12 +0200

    Copy app boilerplate files on install

---
+++ Boilerplate/index.php
@@ -0,0 +1,4 @@
+<?php
+require_once 'vendor/autoload.php';
+
+\AppZap\PHPFramework\Bootstrap::bootstrap();
--- Classes/Plugin.php
+++ Classes/Plugin.php
@@ -6,32 +6,49 @@ use Composer\IO\IOInterface;
 use Composer\Plugin\PluginInterface;
 use Composer\Script\Event;
 use Composer\Script\ScriptEvents;
+use Composer\Util\Filesystem;
 
 class Plugin implements PluginInterface {
 
-    public function activate(Composer $composer, IOInterface $io) {
-        $io->write('activated');
+  protected function recurse_copy($src,$dst) {
+    $dir = opendir($src);
+    @mkdir($dst);
+    while(false !== ( $file = readdir($dir)) ) {
+      if (( $file != '.' ) && ( $file != '..' )) {
+        if ( is_dir($src . '/' . $file) ) {
+          $this->recurse_copy($src . '/' . $file,$dst . '/' . $file);
+        }
+        else {
+          copy($src . '/' . $file,$dst . '/' . $file);
+        }
+      }
     }
+    closedir($dir);
+  }
 
-    public static function getSubscribedEvents() {
-        return [
-            ScriptEvents::POST_INSTALL_CMD => 'postInstall',
-            ScriptEvents::POST_AUTOLOAD_DUMP => 'postAutoload',
-        ];
-    }
+  public function activate(Composer $composer, IOInterface $io) {
+    $this->recurse_copy(dirname(__DIR__ . '/../Boilerplate/*'), '.');
+  }
 
-    /**
-	 * @param \Composer\Script\Event $event
-	 */
-    public function postInstall(Event $event) {
-        echo 'caught event:' . $event->getName();
-    }
-    
-    /**
-	 * @param \Composer\Script\Event $event
-	 */
-	public function postAutoload(Event $event) {
-		echo 'caught event:' . $event->getName();
-	}
+  public static function getSubscribedEvents() {
+    return [
+      ScriptEvents::POST_INSTALL_CMD => 'postInstall',
+      ScriptEvents::POST_AUTOLOAD_DUMP => 'postAutoload',
+    ];
+  }
+
+  /**
+   * @param \Composer\Script\Event $event
+   */
+  public function postInstall(Event $event) {
+    echo 'caught event:' . $event->getName();
+  }
+
+  /**
+   * @param \Composer\Script\Event $event
+   */
+  public function postAutoload(Event $event) {
+    echo 'caught event:' . $event->getName();
+  }
 
 }
